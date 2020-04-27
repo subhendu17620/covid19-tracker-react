2d9ef943a81fe4ade262e18d4cc11c4d98cb2210
subhendu17620 <subhendu17620@gmail.com>
2020-04-27 07:28:52 +0530

    Updates

---
+++ precache-manifest.0061f61bfa7bde3290e1128e311a1e39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fbf6cae11b00358f12bd6276e3b128",
+    "url": "/covid19-tracker-react/index.html"
+  },
+  {
+    "revision": "f4bb72cf21a26496af31",
+    "url": "/covid19-tracker-react/static/css/main.dbefd677.chunk.css"
+  },
+  {
+    "revision": "c55e68f05e86af08f32d",
+    "url": "/covid19-tracker-react/static/js/2.7e230d21.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker-react/static/js/2.7e230d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4bb72cf21a26496af31",
+    "url": "/covid19-tracker-react/static/js/main.4f1b61be.chunk.js"
+  },
+  {
+    "revision": "8136ba3f505656f5de07",
+    "url": "/covid19-tracker-react/static/js/runtime-main.0021f902.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker-react/static/media/image.d7265326.png"
+  }
+]);
